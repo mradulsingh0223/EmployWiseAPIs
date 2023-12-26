cecbc2376a38b74e13e1088a1e2ee61729a67206
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2023-12-26 23:00:48 +0530

    Added Validators

--- src/main/java/com/assesment/employwise_api/model/Employee.java
+++ src/main/java/com/assesment/employwise_api/model/Employee.java
@@ -1,8 +1,12 @@
 package com.assesment.employwise_api.model;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
@@ -13,9 +17,21 @@ import org.springframework.data.annotation.Id;
 public class Employee {
     @org.springframework.data.annotation.Id
     private String employeeId;
+    @NotNull
+    @NotBlank(message="Please enter your name")
     private String name;
+    @NotNull
+    @NotBlank(message="Please enter your Mobile Number")
+    @Digits(message="Number should contain 10 digits.", fraction = 0, integer = 10)
     private int mobileNum;
+    @NotNull
+    @NotBlank(message="Please enter your Mail")
+    @Email(message="Enter valid Email Id.")
     private String mail;
+    @NotNull
+    @NotBlank(message="Please enter the Id of manager you have to report to")
     private String reportId;
+    @NotNull
+    @NotBlank(message="Please enter your Image url")
     private String image;
 }
